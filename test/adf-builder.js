c57e9dc14f528f6fc87719a2c783fcdfdfb217f9
Graham Esau <gesau@hotmail.co.uk>
2018-01-11 13:27:18 +0000

    Add tests for adf-builder compatability

---
+++ test/adf-builder.js
@@ -0,0 +1,22 @@
+import test from 'tape-catch'
+import { doc, paragraph, strong, emoji } from '../src/index'
+import { Paragraph as AParagraph, strong as astrong, emoji as aemoji } from 'adf-builder'
+
+test('adf-builder compatability # including adf-builder block node', t => {
+  const actual = doc(new AParagraph().strong('strong').text('text').toJSON(), emoji('grin'))
+
+  const expected = doc(paragraph(strong('strong'), 'text'), emoji('grin'))
+
+  t.deepEqual(actual, expected)
+  t.end()
+})
+
+test('adf-builder compatability # including adf-builder inline nodes', t => {
+  // TODO does emoji shortName definitely require colons?
+  const actual = doc(paragraph(astrong('strong').toJSON(), 'text'), aemoji(':grin:').toJSON())
+
+  const expected = doc(paragraph(strong('strong'), 'text'), emoji('grin'))
+
+  t.deepEqual(actual, expected)
+  t.end()
+})
