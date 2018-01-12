33287b84467eaa2c062bac7cd443fe476419679c
Graham Esau <gesau@hotmail.co.uk>
2018-01-12 21:41:10 +0000

    Allow specifying codeBlock language

--- src/codeBlock.js
+++ src/codeBlock.js
@@ -1,7 +1,15 @@
 import DocFormatError from './DocFormatError'
 import text from './text'
 
-export default function codeBlock (...content) {
+const languages = ['abap', 'actionscript', 'ada', 'arduino', 'autoit', 'c', 'c++', 'clojure', 'coffeescript', 'csharp', 'css', 'cuda', 'd', 'dart', 'delphi', 'elixir', 'erlang', 'fortran', 'foxpro', 'go', 'groovy', 'haskell', 'haxe', 'html', 'java', 'javascript', 'json', 'julia', 'kotlin', 'latex', 'livescript', 'lua', 'mathematica', 'matlab', 'objective-c', 'objective-j', 'objectpascal', 'ocaml', 'octave', 'perl', 'php', 'powershell', 'prolog', 'puppet', 'python', 'qml', 'r', 'racket', 'restructuredtext', 'ruby', 'rust', 'sass', 'scala', 'scheme', 'shell', 'smalltalk', 'sql', 'standardml', 'swift', 'tcl', 'tex', 'typescript', 'vala', 'vbnet', 'verilog', 'vhdl', 'xml', 'xquery']
+const languageAliases = { cpp: 'c++', 'c#': 'csharp', objectivec: 'objective-c', objectivej: 'objective-j' }
+
+export default function codeBlock (language, ...content) {
+  language = languageAliases[language] || language
+  if (language && languages.indexOf(language) === -1) {
+    throw new DocFormatError(`Invalid codeBlock language '${language}' - must be one of ${languages.join(',')}`)
+  }
+
   const textContent = content.map(c => typeof c === 'string' ? text(c) : c)
 
   for (const node of textContent) {
@@ -13,8 +21,20 @@ export default function codeBlock (...content) {
     }
   }
 
-  return {
+  const node = {
     type: 'codeBlock',
     content: textContent
   }
+  if (language) {
+    node.attrs = { language }
+  }
+  return node
 }
+
+for (const language of languages) {
+  codeBlock[language] = (...content) => codeBlock(language, ...content)
+}
+for (const [alias, language] of Object.entries(languageAliases)) {
+  codeBlock[alias] = (...content) => codeBlock(language, ...content)
+}
+codeBlock.plain = (...content) => codeBlock(null, ...content)
--- test/codeBlock.js
+++ test/codeBlock.js
@@ -2,7 +2,7 @@ import test from 'tape-catch'
 import { codeBlock, paragraph, strong, emoji, text, DocFormatError } from '../src/index'
 
 test('Code Block # Different item types', t => {
-  const p = codeBlock('plain', text('text'))
+  const actual = codeBlock(null, 'plain', text('text'))
 
   const expected = {
     'type': 'codeBlock',
@@ -18,34 +18,106 @@ test('Code Block # Different item types', t => {
     ]
   }
 
-  t.deepEqual(p, expected)
+  t.deepEqual(actual, expected)
+  t.end()
+})
+
+test('Code Block # Plain text with helper function', t => {
+  const actual = codeBlock.plain('text')
+
+  const expected = {
+    'type': 'codeBlock',
+    'content': [
+      {
+        'type': 'text',
+        'text': 'text'
+      }
+    ]
+  }
+
+  t.deepEqual(actual, expected)
+  t.end()
+})
+
+test('Code Block # Can specify language in first parameter', t => {
+  const actual = codeBlock('javascript', 'text')
+
+  const expected = {
+    'type': 'codeBlock',
+    'content': [
+      {
+        'type': 'text',
+        'text': 'text'
+      }
+    ],
+    'attrs': {
+      'language': 'javascript'
+    }
+  }
+
+  t.deepEqual(actual, expected)
+  t.end()
+})
+
+test('Code Block # Can specify language with helper function', t => {
+  const actual = codeBlock.javascript('text')
+
+  const expected = {
+    'type': 'codeBlock',
+    'content': [
+      {
+        'type': 'text',
+        'text': 'text'
+      }
+    ],
+    'attrs': {
+      'language': 'javascript'
+    }
+  }
+
+  t.deepEqual(actual, expected)
+  t.end()
+})
+
+test('Code Block # Some languages have aliases', t => {
+  t.deepEqual(codeBlock('cpp'), codeBlock('c++'))
+  t.deepEqual(codeBlock('csharp'), codeBlock('c#'))
+  t.deepEqual(codeBlock('objectivec'), codeBlock('objective-c'))
+  t.deepEqual(codeBlock('objectivej'), codeBlock('objective-j'))
+
   t.end()
 })
 
 test('Code Block # Can be empty', t => {
-  const p = codeBlock()
+  const actual = codeBlock()
 
   const expected = {
     'type': 'codeBlock',
     'content': []
   }
 
-  t.deepEqual(p, expected)
+  t.deepEqual(actual, expected)
+  t.end()
+})
+
+test('Code Block # Must be given a valid language', t => {
+  t.throws(() => codeBlock('esperanto', 'text'), DocFormatError)
+  t.throws(() => codeBlock(text('text')), DocFormatError)
   t.end()
 })
 
 test('Code Block # Text must not be marked', t => {
-  t.throws(() => codeBlock('plain', strong('formatted')), DocFormatError)
-  t.throws(() => codeBlock(strong('formatted')), DocFormatError)
+  t.throws(() => codeBlock(null, 'plain', strong('formatted')), DocFormatError)
+  t.throws(() => codeBlock(null, strong('formatted')), DocFormatError)
   t.end()
 })
 
 test('Code Block # Must be given string or text node', t => {
-  t.throws(() => codeBlock(paragraph()), DocFormatError)
-  t.throws(() => codeBlock(emoji()), DocFormatError)
-  t.throws(() => codeBlock(1), DocFormatError)
-  t.throws(() => codeBlock([]), DocFormatError)
-  t.throws(() => codeBlock({}), DocFormatError)
-  t.throws(() => codeBlock(null), DocFormatError)
+  t.throws(() => codeBlock(null, paragraph()), DocFormatError)
+  t.throws(() => codeBlock(null, emoji()), DocFormatError)
+  t.throws(() => codeBlock(null, 1), DocFormatError)
+  t.throws(() => codeBlock(null, []), DocFormatError)
+  t.throws(() => codeBlock(null, {}), DocFormatError)
+  t.throws(() => codeBlock(null, null), DocFormatError)
   t.end()
 })
