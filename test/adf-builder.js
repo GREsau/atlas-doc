69f6f3adcd939abf97db1211630470da781e662f
Graham Esau <gesau@hotmail.co.uk>
2018-01-23 10:13:53 +0000

    Don't run adf-builder tests in node versions <6

--- test/adf-builder.js
+++ test/adf-builder.js
@@ -1,21 +1,26 @@
 import test from 'tape-catch'
 import { doc, paragraph, strong, emoji } from '../src/index'
-import * as adf from 'adf-builder'
 
-test('adf-builder compatability # including adf-builder block node', t => {
-  const actual = doc(new adf.Paragraph().strong('strong').text('text').toJSON(), emoji('grin'))
+// adf-builder requires node 6.0.0 or greater
+const nodeMajorVersion = parseInt(process.version.match(/\d+/)[0])
+if (nodeMajorVersion >= 6) {
+  const adf = require('adf-builder')
 
-  const expected = doc(paragraph(strong('strong'), 'text'), emoji('grin'))
+  test('adf-builder compatability # including adf-builder block node', t => {
+    const actual = doc(new adf.Paragraph().strong('strong').text('text').toJSON(), emoji('grin'))
 
-  t.deepEqual(actual, expected)
-  t.end()
-})
+    const expected = doc(paragraph(strong('strong'), 'text'), emoji('grin'))
 
-test('adf-builder compatability # including adf-builder inline nodes', t => {
-  const actual = doc(paragraph(adf.strong('strong').toJSON(), 'text'), adf.emoji(':grin:').toJSON())
+    t.deepEqual(actual, expected)
+    t.end()
+  })
 
-  const expected = doc(paragraph(strong('strong'), 'text'), emoji('grin'))
+  test('adf-builder compatability # including adf-builder inline nodes', t => {
+    const actual = doc(paragraph(adf.strong('strong').toJSON(), 'text'), adf.emoji(':grin:').toJSON())
 
-  t.deepEqual(actual, expected)
-  t.end()
-})
+    const expected = doc(paragraph(strong('strong'), 'text'), emoji('grin'))
+
+    t.deepEqual(actual, expected)
+    t.end()
+  })
+}
